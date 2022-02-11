0c0729f6e0f259820d7b5597482a9a51bb378208
Jun Nakayama <jnakayama.re@gmail.com>
2022-02-11 15:55:57 +0900

    Update Reanalysis of clinical TNBC.R

--- Reanalysis of clinical TNBC.R
+++ Reanalysis of clinical TNBC.R
@@ -1,113 +1,167 @@
 library(Seurat)
 library(harmony)
-library(ComplexHeatmap)
-library(scater)
-library(Matrix)
-library(tidyverse)
 library(dplyr)
 library(tidyr)
 library(reshape2)
-library(pheatmap)
-library(Rtsne)
-library(cowplot)
 library(ggplot2)
-library(ggsci)
-library(scales)
 library(MAST)
-library(DOSE)
-library(patchwork)
-library(plotly)
-library(monocle)
-library(MASS)
-library(loomR)
-library(RColorBrewer)
-library(grDevices)
-library(colorRamps)
-library(data.table)
-library(hexbin)
+
 
 
 setwd("~/Analysis/STmeta")
 
 
-data <- read.delim2("GSE118389_tpm_rsem.txt", header = TRUE)
-TNBC <- CreateSeuratObject(counts = data, project = "TNBC")
-TNBC <- NormalizeData(TNBC)
-TNBC[["percent.mt"]] <- PercentageFeatureSet(TNBC, pattern = "^MT-")
-TNBC <- FindVariableFeatures(TNBC, selection.method = "vst")
-TNBC <- ScaleData(TNBC, verbose = FALSE)
+####### GSE163210
+
+
+T1 <-  Read10X_h5("~/Analysis/STmeta/Revise/GSE163210_RAW/GSM4974849_T1.h5")
+T2 <-  Read10X_h5("~/Analysis/STmeta/Revise/GSE163210_RAW/GSM4974849_T1.h5")
+T3 <-  Read10X_h5("~/Analysis/STmeta/Revise/GSE163210_RAW/GSM4974851_T3.h5")
+CTC1 <-  Read10X_h5("~/Analysis/STmeta/Revise/GSE163210_RAW/GSM4974852_CTC1.h5")
+CTC2 <-  Read10X_h5("~/Analysis/STmeta/Revise/GSE163210_RAW/GSM4974853_CTC2.h5")
+
+T1 <- CreateSeuratObject(T1, project = "GSE163210_T1")
+T2 <- CreateSeuratObject(T2, project = "GSE163210_T2")
+T3 <- CreateSeuratObject(T3, project = "GSE163210_T3")
+CTC1 <- CreateSeuratObject(CTC1, project = "GSE163210_CTC1")
+CTC2 <- CreateSeuratObject(CTC2, project = "GSE163210_CTC2")
+
+
+GSE163210 <- merge(T1, y = c(T2, T3, CTC1, CTC2))
+
+
+GSE163210 <- NormalizeData(GSE163210, normalization.method = "LogNormalize", scale.factor = 10000)
+GSE163210 <- FindVariableFeatures(GSE163210)
+GSE163210[["percent.mt"]] <- PercentageFeatureSet(GSE163210, pattern = "MT-")
+GSE163210 <- ScaleData(GSE163210, vars.to.regress = "percent.mt")
+GSE163210 <- RunPCA(GSE163210, npcs = 100, ndims.print = 1:5, nfeatures.print = 5)
+GSE163210 <- FindNeighbors(GSE163210, reduction = "pca", dims = 1:75, nn.eps = 0.5)
+GSE163210 <- FindClusters(GSE163210, resolution = 3, n.start = 10)
+GSE163210 <- RunUMAP(GSE163210, dims = 1:75, min.dist = 0.75)
+
+DimPlot(GSE163210)
+
+saveRDS(GSE163210, file = "GSE163210.rds")
+
+
+
+FeaturePlot(GSE163210, features = c("HMGA1", "CD44"), blend = TRUE)
+
+
+
+
+# > GSE163210
+# An object of class Seurat 
+# 54328 features across 8494 samples within 1 assay 
+# Active assay: RNA (54328 features, 2000 variable features)
+#  2 dimensional reductions calculated: pca, umap
+
+
+gene <- c("HMGA1", "CD44")
+obj <- GSE163210[unlist(gene),]
+mat <- t(as.matrix(GetAssayData(obj)))
+write.table(mat, file = "HMGA1CD44-PDX.csv", sep = ",")
+
+
+write.table(GSE163210@meta.data, file = "GSE163210meta.csv", sep = ",")
+
+
+
+
+
+
+
+
+
+#### Integrated dataset
+GSE118389 <- readRDS("./GSE118389.rds")
+GSE161529 <- readRDS("./GSE161529.rds")
+GSE176078 <- readRDS("./GSE176078.rds")
+GSE180286 <- readRDS("./GSE180286.rds")
+Yamamotolab <- readRDS("./Yamamotolab.rds")
+
+D <- merge(GSE118389, y = c(GSE161529, GSE176078, GSE180286, Yamamotolab), merge.data = TRUE)
+
+
+metadata <- read.csv("meta-predata.csv")
+metadata <- data.frame(metadata)
+rownames(metadata) <- metadata[,1]
+
+D@meta.data <- metadata
+
+cl = c("Primary", "Metastasis", "DCIS")
+D2 <- subset(D, subset = Tissue_state %in% cl)
+
+D2 <- NormalizeData(D2)
+VlnPlot(D2, features = "percent.mt", group.by = "Dataeset")
+D2 <- subset(D2, subset = nFeature_RNA > 500 & percent.mt < 20)
+
+
+D2 <- FindVariableFeatures(D2)
+D2 <- ScaleData(D2, vars.to.regress = "percent.mt")
+
 s.genes <- cc.genes$s.genes
 g2m.genes <- cc.genes$g2m.genes
-ST.combined <- CellCycleScoring(TNBC, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
-TNBC <- RunPCA(TNBC, npcs = 100, ndims.print = 1:5)
-DimPlot(TNBC, reduction = "pca")
-DimHeatmap(TNBC, dims = 1:15, cells = 500, balanced = TRUE)
+D2 <- CellCycleScoring(D2, s.features = s.genes, g2m.features = g2m.genes, set.ident = TRUE)
 
 
-#######################
-TNBC <- RunUMAP(TNBC, dims = 1:25, min.dist = 0.5)
-TNBC <- RunTSNE(TNBC, dims = 1:15, check_duplicates = FALSE)
-TNBC <- FindNeighbors(TNBC, dims = 1:15)
+
+D2 <- RunPCA(D2, npcs = 100, ndims.print = 1:5, nfeatures.print = 5)
+D2 <- RunHarmony(D2, "orig.ident")
+
+D2 <- RunUMAP(D2, reduction = "harmony", dims = 1:100, min.dist = 0.5)
+D2 <- FindNeighbors(D2, dims = 1:15, reduction = "harmony")
+D2 <- FindClusters(D2, resolution = 1)
+
+
+
+saveRDS(D2, file = "BRCAonly.rds")
+
+
+
+
+### select the TNBC patients from three datasets
+
+TNBC <- subset(BRCAonly, subset = subtype == "TNBC")
+
+TNBC <- RunUMAP(TNBC, reduction = "harmony", dims = 1:100, min.dist = 0.5)
+TNBC <- FindNeighbors(TNBC, dims = 1:15, reduction = "harmony")
 TNBC <- FindClusters(TNBC, resolution = 1)
 
-DimPlot(TNBC, reduction = "tsne", pt.size = 2)
+DimPlot(TNBC)
 
 saveRDS(TNBC, file = "TNBC.rds")
 
 
 
-ALL.markers <- FindAllMarkers(TNBC, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.5, test.use = "wilcox")
-write.table(ALL.markers, file = "ALLmarkers.csv", sep = ",")
 
 
-#######################
-## Subset Epithelial cancer cell
-EPI <- subset(TNBC, ident = c(2,3,5,7,8))
-EPI <- RunUMAP(EPI, dims = 1:25, min.dist = 0.5)
-EPI <- RunTSNE(EPI, dims = 1:15, check_duplicates = FALSE)
-EPI <- FindNeighbors(EPI, dims = 1:15)
+
+sel = c(0, 3, 6, 7, 11, 12, 15, 16, 19, 20, 21, 23, 24)
+EPI = subset(TNBC, ident = sel)
+
+EPI <- RunUMAP(EPI, reduction = "harmony", dims = 1:100, min.dist = 0.5)
+EPI <- FindNeighbors(EPI, dims = 1:15, reduction = "harmony")
 EPI <- FindClusters(EPI, resolution = 1)
 
-DimPlot(EPI, pt.size = 2.5)
-DimPlot(EPI, reduction = "tsne", pt.size = 2.5)
 
-EPI.markers <- FindAllMarkers(EPI, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.5, test.use = "wilcox")
-write.table(EPI.markers, file = "EPImarkers.csv", sep = ",")
-
-# 0 CCL20
-# 1 CDH2
-# 2 TBX3
-# 3 RNASE1
-# 4 PDGFA
-# 5 CCL28
-saveRDS(EPI, file = "EPI.rds")
+DimPlot(EPI)
+DimPlot(EPI, group.by = "Tissue_state")
 
 
-### Module analysis
-# use the gene signatures
-# list1 is HMGA1 signatures
-# list2 is CD44 signatures
-list <- read.csv("list1.csv", header = FALSE)
-list = unlist(list)
-allgene = rownames(EPI)
-list1 = list(allgene[allgene %in% unlist(list)])
-
-list2 <- read.csv("list2.csv", header = FALSE)
-list2 = unlist(list2)
-allgene = rownames(EPI)
-list2 = list(allgene[allgene %in% unlist(list2)])
 
 
-mod1 <- AddModuleScore(EPI, features = list1, pool = allgene, name = "HMGA1sig.")
-mod1 <- AddModuleScore(mod1, features = list2, pool = allgene, name = "CD44sig.")
+FeaturePlot(EPI, "HMGA1")
+FeaturePlot(EPI, "CD44")
+FeaturePlot(EPI, features = c("HMGA1", "CD44"), blend = TRUE)
+FeatureScatter(EPI, feature1 = "HMGA1", feature2 = "CD44", group.by = "orig.ident")
 
-write.table(as.matrix(mod1@meta.data[,7:8]), file = "signature.csv", sep = ",")
 
-FeaturePlot(mod1, features = "HMGA1sig.1", cols = c("grey", "orange","red2") , pt.size = 2)
-FeaturePlot(mod1, features = "CD44sig.1", cols = c("grey", "orange","red2") , pt.size = 2)
-VlnPlot(mod1, features = "HMGA1sig.1")
-VlnPlot(mod1, features = "CD44sig.1")
-VlnPlot(subset(mod1, ident = c(2,3,5,7,8)), features = "HMGA1sig.1", pt.size = 2)
-FeatureScatter(mod1, feature1 = "HMGA1sig.1", feature2 = "CD44sig.1", pt.size = 2)
-FeaturePlot(mod1, features = c("HMGA1sig.1", "CD44sig.1"), blend = TRUE, pt.size = 2.5)
+
+
+
+
+
+
+####### GSE163210
 
